86df662f1d733cb1a6d28935366ff87f70925440
Muhammed Faris P <102180559+farisp123@users.noreply.github.com>
2023-06-17 23:29:51 +0530

    Updated constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 export const baseUrl= 'https://api.themoviedb.org/3';
-export const API_KEY ='4aff956eeaf81d34a6a95d876cde79ee';
+export const API_KEY ='API_KEY'; //Place your API_KEY
 export const imageUrl ='https://image.tmdb.org/t/p/original';
 
